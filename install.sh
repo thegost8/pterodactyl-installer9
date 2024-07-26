a2b489439314cfc0011a233d0580fcbf0ef092d2
thegost8 <157043863+thegost8@users.noreply.github.com>
2024-07-26 02:28:48 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v1.1.0"
 export SCRIPT_RELEASE="v1.1.0"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/thegost8/pterodactyl-installer9"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
